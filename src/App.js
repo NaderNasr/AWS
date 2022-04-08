3ec9c3ba75dc3f3f35b1dd48c53403653da60299
NaderNasr <nadernasr7@gmail.com>
2022-04-07 23:30:26 -0400

    testing aws api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { Button, Card } from 'react-bootstrap';
+
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -7,18 +9,22 @@ import '@aws-amplify/ui-react/styles.css';
 function App() {
   return (
     <div>
-      <Authenticator>
+      <Authenticator className="App">
         {({ signOut, user }) => (
-          <div className="App">
-            <p>
-              Hey {user.username}! welcome to my site, with auth!
-            </p>
-            <ul>
+          <div  className="App">
+            <Card>
+              <Card.Body>
+                <Card.Title>Hey {user.username}! welcome to my site</Card.Title>
+                <Card.Text>
+                <ul>
               <li>Username: {user.username}</li>
               <li>Email: {user.attributes.email}</li>
               <li>Verified: {user.attributes.email_verified ? 'Email has been verified :)' : 'Not yet verified :('}</li>
             </ul>
-            <button onClick={signOut}>Sign out</button>
+                </Card.Text>
+              </Card.Body>
+              <Button style={{display:'flex', alignItems:'center'}} variant="danger" onClick={signOut}>Sign out</Button>
+            </Card>
           </div>
         )}
       </Authenticator>
